19069dcb796eb91479e94048151ae3ffa3cb870e
tshrum <trisha.shrum@colorado.edu>
2017-07-19 12:04:20 -0600

    Taking out this code because the herd never goes to 0 and who knows if it may be causing a problem. (aka desperate attempt to fix mystery problem)

--- R/simUI.R
+++ R/simUI.R
@@ -431,13 +431,6 @@ simCreator <- function(input, output, session, i, rv, simLength, startYear, myOu
   output[[paste0("insuranceUpdate", name)]] <- renderUI({
     if(!is.null(input[[paste0("year", name, "Summer")]])){
        if(input[[paste0("year", name, "Summer")]] == 1){
-         if(myOuts[i, herd] == 0){
-           if(orgName == "prac"){
-             indem[[i]]$indemnity <<- 0
-           }else{
-             indem[[i]]$indemnity <<- 0
-           }
-         }
         currentIndem <- prettyNum(indem[[i]]$indemnity, digits = 0, big.mark=",",scientific=FALSE)
         tagList(
           br(),
